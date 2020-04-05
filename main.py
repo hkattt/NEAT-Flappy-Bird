708d3d51e18ae3d7ba8e394555da6c922702ce36
hkattt <2690133@schoolsnet.act.edu.au>
2020-04-05 16:32:21 +1000

    got some AI to work it seems

--- main.py
+++ main.py
@@ -15,29 +15,43 @@ pg.display.set_caption("Flappy Bird")
 
 class Game():
     def __init__(self):
-        self.running = True
         pg.init()
         self.background = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption("Flappy Bird")
         self.clock = pg.time.Clock()
         self.score = 0
-        self.score_font = pg.font.SysFont("comicsans", 30)
+        self.generation = 0
+        self.font = pg.font.SysFont("comicsans", 30)
 
-    def new(self):
+    def new(self, genomes, config):
+        self.generation += 1
+        self.reset()
         # sprite groups
         self.all_sprites = pg.sprite.Group()
         self.birds = pg.sprite.Group()
         self.pipes = pg.sprite.Group()
 
+        # start by creating lists holding the genome itself and the
+        # neural network associated with the genome
+        self.nets = []
+        self.bird_indices = [] # allows us to keep track of what bird coresponds to which genome and neural network
+        self.ge = []
+
+        for genome_id, genome in genomes:
+            genome.fitness = 0  # start with fitness level of 0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            self.nets.append(net)
+            self.bird_indices.append(Bird(WIDTH / 2, HEIGHT / 2, self))
+            self.ge.append(genome)
+
         # creates objects
-        self.bird = Bird(WIDTH / 2, HEIGHT / 2, self)
         self.base = Base(400, self)
         self.pipe = Pipe(400, self)
         self.run()
 
     def run(self):
         self.playing = True
-        while self.playing:
+        while self.playing and len(self.bird_indices) > 0:
             self.clock.tick(FPS)
             self.update()
             self.events()
@@ -66,13 +80,42 @@ class Game():
         for sprite in self.birds:
             self.background.blit(sprite.rotated_img, sprite.new_rect)
          # score
-        text = self.score_font.render("Score: " + str(self.score), 1, WHITE)
-        background.blit(text, (WIDTH - 10 - text.get_width(), 10))
+        score = self.font.render("Score: " + str(self.score), 1, WHITE)
+        background.blit(score, (WIDTH - 10 - score.get_width(), 10))
+        # generation
+        generation = self.font.render("Generation: " + str(self.generation), 1, WHITE)
+        background.blit(generation, (10, 10))
         pg.display.update()
 
+    def reset(self):
+        self.score = 0
+
 game = Game()
-while game.running:
-    game.new()
-pg.quit()
-quit()
+
+
+def run(config_file):
+    """ runs the NEAT algorithm to train a neural network to play flappy bird """
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_file)
+
+    # Create the population, which is the top-level object for a NEAT run.
+    population = neat.Population(config)
+
+    # Add a stdout reporter to show progress in the terminal.
+    population.add_reporter(neat.StdOutReporter(True))
+    population.add_reporter(neat.StatisticsReporter())
+
+    # Run for up to 50 generations.
+    winner = population.run(game.new, 50)
+
+    # show final stats
+    print('\nBest genome:\n{!s}'.format(winner))
+
+
+if __name__ == '__main__':
+    # Determine path to configuration file. This path manipulation is
+    # here so that the script will run successfully regardless of the
+    # current working directory.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    run(config_path)
 
--- sprites.py
+++ sprites.py
@@ -36,7 +36,9 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.img.get_rect()
 
     def update(self):
+        self.fitness_award()
         self.move()
+        self.neural_network_choice()
         self.animation()
 
     def jump(self):
@@ -93,6 +95,15 @@ class Bird(pg.sprite.Sprite):
         # Creates an image mask for collisions
         self.mask = pg.mask.from_surface(self.img)
 
+    def fitness_award(self):
+        self.game.ge[self.game.bird_indices.index(self)].fitness += 1
+
+    def neural_network_choice(self):
+        # send bird location, top pipe location and bottom pipe location and determine from network whether to jump or not
+        output = self.game.nets[self.game.bird_indices.index(self)].activate((self.y, abs(self.y - self.game.pipe.height), abs(self.y - self.game.pipe.bottom)))
+
+        if output[0] > 0.5:  # we use a tanh activation function so result will be between -1 and 1. if over 0.5 jump
+            self.jump()
 class Pipe(pg.sprite.Sprite):
     def __init__(self, x, game):
         self.groups = game.all_sprites, game.pipes # pipe groups
@@ -124,10 +135,15 @@ class Pipe(pg.sprite.Sprite):
             self.game.all_sprites.remove(self)
             self.game.pipes.remove(self)
         # if the bird passed the pipe a new pipe is created
-        if not self.passed and self.x < self.game.bird.x:
-            self.game.pipe = Pipe(400, self.game)
-            self.passed = True
-
+        if len(self.game.bird_indices) > 0:
+            if not self.passed and self.x < self.game.bird_indices[0].x:
+                self.game.score += 1
+                # birds get awarded for passing a pipe
+                for genome in self.game.ge:
+                    genome.fitness += 5
+                self.game.pipe = Pipe(400, self.game)
+                self.passed = True
+            
     def set_height(self):
         self.height = random.randrange(50, 260)
         self.top = self.height - self.pipe_top.get_height()
@@ -141,18 +157,26 @@ class Pipe(pg.sprite.Sprite):
         top_mask = pg.mask.from_surface(self.pipe_top)
         bottom_mask = pg.mask.from_surface(self.pipe_bottom)
 
-        # offset between the masks
-        top_offset = (self.x - round(self.game.bird.x), self.top - round(self.game.bird.y))
-        bottom_offset = (self.x - round(self.game.bird.x), self.bottom - round(self.game.bird.y))
+        for bird in self.game.birds:
+            # offset between the masks
+            top_offset = (self.x - round(bird.x), self.top - round(bird.y))
+            bottom_offset = (self.x - round(bird.x), self.bottom - round(bird.y))
 
-        # calculates the point of collision
-        point_of_collision_t = self.game.bird.mask.overlap(top_mask, top_offset)
-        point_of_collision_b = self.game.bird.mask.overlap(bottom_mask, bottom_offset)
+            # calculates the point of collision
+            point_of_collision_t = bird.mask.overlap(top_mask, top_offset)
+            point_of_collision_b = bird.mask.overlap(bottom_mask, bottom_offset)
 
-        # checks if there was a collision between either mask
-        if point_of_collision_t or point_of_collision_b:
-            return True
-        return False
+            # checks if there was a collision between either mask
+            if point_of_collision_t or point_of_collision_b:
+                # removes fitness
+                self.game.ge[self.game.bird_indices.index(bird)].fitness -= 1
+                # removes bird from lists
+                self.game.nets.pop(self.game.bird_indices.index(bird))
+                self.game.ge.pop(self.game.bird_indices.index(bird))
+                self.game.bird_indices.pop(self.game.bird_indices.index(bird))
+                # removes bird from sprite groups
+                self.game.all_sprites.remove(bird)
+                self.game.birds.remove(bird)
 
 class Base(pg.sprite.Sprite):
     def __init__(self, y, game):
@@ -170,8 +194,7 @@ class Base(pg.sprite.Sprite):
 
     def update(self):
         self.move()
-        if self.collisions():
-            pass
+        self.collisions()
 
     def move(self):
         self.x1 -= BASE_VEL
@@ -184,7 +207,12 @@ class Base(pg.sprite.Sprite):
             self.x2 = self.x1 + self.width
 
     def collisions(self):
-        if self.game.bird.y + self.game.bird.img.get_height() > self.y or self.game.bird.y < 0:
-            return True
-
-    
+        for bird in self.game.birds:
+            if bird.y + bird.img.get_height() > self.y or bird.y < 0:
+                # removes from list
+                self.game.nets.pop(self.game.bird_indices.index(bird))
+                self.game.ge.pop(self.game.bird_indices.index(bird))
+                self.game.bird_indices.pop(self.game.bird_indices.index(bird))
+                # remove from sprite groups
+                self.game.all_sprites.remove(bird)
+                self.game.birds.remove(bird)
